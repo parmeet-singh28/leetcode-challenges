e3947058b5ed3896dfdb6f3269bd4323c6f55064
Parmeet Singh <109502711+parmeet-singh28@users.noreply.github.com>
2026-07-11 15:25:56 +0530

    Time: 153 ms (5.07%), Space: 179.6 MB (5.02%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
-    void bfs(unordered_map<int, list<int>> &adj, int nd, vector<int> &viss, int &nodesCnt, int &edgesCnt, int n){
-       vector<vector<int>> vis(n, vector<int>(n, 0));
-       unordered_set<int> st;
+    unordered_set<int> st;
+    void bfs(unordered_map<int, list<int>> &adj, int nd, vector<int> &viss, vector<vector<int>> &vis, int &nodesCnt, int &edgesCnt, int n){
+       
+       st.clear();
        queue<int> q;
        q.push(nd);
        while(!q.empty()){
@@ -10,10 +11,6 @@ public:
             q.pop();
             viss[node] = 1;
             st.insert(node);
-            nodesCnt++;
-            // if(nd==0){
-            //     cout<<node<<endl;
-            // }
 
             for(auto nbr:adj[node]){
                 if(!vis[node][nbr] && !vis[nbr][node]){
@@ -34,11 +31,12 @@ public:
             adj[v].push_back(u);
        }
        vector<int> viss(n, 0);
+       vector<vector<int>> vis(n, vector<int>(n, 0));
        int ans = 0;
        for(int i=0; i<n; i++){
             if(!viss[i]){
                 int nodesCnt = 0, edgesCnt = 0;
-                bfs(adj, i, viss, nodesCnt, edgesCnt, n);
+                bfs(adj, i, viss, vis, nodesCnt, edgesCnt, n);
                 // cout<<i<<" "<<nodesCnt<<" "<<edgesCnt<<endl;
                 if((nodesCnt*(nodesCnt-1))/2 == edgesCnt) ans++;
             }
