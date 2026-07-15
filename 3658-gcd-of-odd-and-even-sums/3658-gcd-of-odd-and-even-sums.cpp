62b27d2a6a905c769708c2b5c890092074dd6bf4
Parmeet Singh <109502711+parmeet-singh28@users.noreply.github.com>
2026-07-15 09:27:40 +0530

    Time: 0 ms (100%), Space: 8.5 MB (46.67%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
         int eSum = n*(n+1);
-        n*=2;
-        int oSum = (n*(n+1))/2-eSum;
+        int oSum = n*n;
         return __gcd(eSum, oSum);
     }
 };
