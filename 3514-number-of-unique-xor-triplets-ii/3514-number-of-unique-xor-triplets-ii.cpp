aa0d86c6237ca17ad9b7b7d7e010ff23002f32fc
Parmeet Singh <109502711+parmeet-singh28@users.noreply.github.com>
2026-07-25 19:31:44 +0530

    Time: 604 ms (66.09%), Space: 35.2 MB (87.74%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        unordered_map<int, bool> mp;
+        const int max_xor = 2048;
+
+        vector<bool> pair_xor(max_xor, false);
+        vector<bool> triplet_xor(max_xor, false);
+
         int n = nums.size();
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                mp[nums[i]^nums[j]] = true;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                pair_xor[nums[i] ^ nums[j]] = true;
             }
         }
-        unordered_set<int> st;
-        for(auto it:mp){
-            int num = it.first;
-            for(auto jt:nums){
-                st.insert(num^jt);
+
+        for (int x = 0; x < max_xor; x++) {
+            if (!pair_xor[x]) continue;
+            for (int v : nums) {
+                triplet_xor[x ^ v] = true;
             }
         }
-        return st.size();
+
+        int count = 0;
+        for(int i = 0; i < max_xor; i++){
+            if(triplet_xor[i]) count++;
+        }
+
+        return count;
     }
 };
